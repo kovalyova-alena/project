61fd9f4c1c6ee94cc29c7490d991f9644f1586e4
kovalyova-alena <kovalyova_alena@inbox.ru>
2016-06-17 00:17:30 +0300

    add to project files bower_components and bower.json write js code for accordion made working accordion without text

--- js/js.js
+++ js/js.js
@@ -1,6 +1,27 @@
 $(document).on('ready', function() {
 
-    $("#modal_submit").on('submit', function(e) { 
+    /*----------------Accordion -------------------*/
+
+    $('div.panel-heading').on('click', function(e) {
+        var currentId = $(this).attr('id');
+        $(".sprite_accordion").css('background-position', 'top');
+        $("#" + currentId +" "+".sprite_accordion").css('background-position', 'bottom');
+        $('.panel-heading').css('background', '#fff');
+        $("#" + currentId).css('background', '#ebc339');
+       
+    });
+
+
+    /*----------------Gallery -------------------*/
+    $(".fancybox")
+        .attr('rel', 'gallery')
+        .fancybox({
+            padding : 7
+        });
+
+
+    /*----------------AJAX Send Form -------------------*/
+        $("#modal_submit").on('submit', function(e) {
     		e.preventDefault();
             var FormData;
             FormData = $(this).serialize(); 
@@ -13,14 +34,13 @@ $(document).on('ready', function() {
                    alert("Your message has been sent!");
             }
             });
-    });
-
-
-    $(".fancybox")
-        .attr('rel', 'gallery')
-        .fancybox({
-            padding : 7
         });
+
+
+
+
+
+
 
 
 });
